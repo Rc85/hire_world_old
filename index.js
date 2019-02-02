814b1d2c1524084f6ae44e55ec42a984e90a53e7
Roger Chin <rogerchin85@gmail.com>
2019-02-01 23:31:14 -0800

    Removed secure props in cookie-session middleware

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ app.use(bodyParser.json({
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
-    maxAge: 8.64e+7,
-    secure: process.env.NODE_ENV === 'development' ? false : true
+    maxAge: 8.64e+7
 }));
 
 app.use(function (req, res, next) {

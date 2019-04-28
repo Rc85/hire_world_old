9a9c883104492c28be0b8e7ae98de5c5ca46a678
Roger Chin <rogerchin85@gmail.com>
2019-04-27 17:47:43 -0700

    Fixed typo in migration

--- migrations/1556358518599_alter-all-dates-to-time-zone.js
+++ migrations/1556358518599_alter-all-dates-to-time-zone.js
@@ -11,7 +11,7 @@ exports.up = pgm => {
     pgm.alterColumn('announcements', 'announcement_created_date', {
         type: 'timestamp with time zone'
     }),
-    pgm.alterColumn('appeal_abandoned_jobs', 'appeal_on', {
+    pgm.alterColumn('appeal_abandoned_jobs', 'appealed_on', {
         type: 'timestamp with time zone'
     }),
     pgm.alterColumn('blocked_users', 'blocked_date', {
@@ -71,7 +71,7 @@ exports.up = pgm => {
     pgm.alterColumn('jobs', 'job_end_date', {
         type: 'timestamp with time zone'
     }),
-    pgm.alterColumn('jobs', 'job_modified_Date', {
+    pgm.alterColumn('jobs', 'job_modified_date', {
         type: 'timestamp with time zone'
     }),
     pgm.alterColumn('jobs', 'job_due_date', {
@@ -80,7 +80,7 @@ exports.up = pgm => {
     pgm.alterColumn('messages', 'message_date', {
         type: 'timestamp with time zone'
     }),
-    pgm.alterColumn('milestone_conditions', 'condition_complete_date', {
+    pgm.alterColumn('milestone_conditions', 'condition_completed_date', {
         type: 'timestamp with time zone'
     }),
     pgm.alterColumn('milestone_files', 'file_uploaded_date', {
@@ -95,7 +95,7 @@ exports.up = pgm => {
     pgm.alterColumn('pinned_messages', 'message_pinned_date', {
         type: 'timestamp with time zone'
     }),
-    pgm.alterColumn('promotions', 'promo_Created_date', {
+    pgm.alterColumn('promotions', 'promo_created_date', {
         type: 'timestamp with time zone'
     }),
     pgm.alterColumn('promotions', 'promo_effective_start_date', {
@@ -116,7 +116,7 @@ exports.up = pgm => {
     pgm.alterColumn('review_tokens', 'token_used_date', {
         type: 'timestamp with time zone'
     }),
-    pgm.alterColumn('saved_job_post', 'saved_date', {
+    pgm.alterColumn('saved_job_posts', 'saved_date', {
         type: 'timestamp with time zone'
     }),
     pgm.alterColumn('sectors', 'sector_created_on', {

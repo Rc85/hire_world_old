ba4152d17058fe1354d478fc6a35fb2185e153fb
Roger Chin <rogerchin85@gmail.com>
2019-05-06 17:04:17 -0700

    Editing cron job

--- cron_jobs/check_milestone_funds.js
+++ cron_jobs/check_milestone_funds.js
@@ -1,4 +1,8 @@
 require('dot-env').config();
 const db = require('../modules/db');
+const error = require('../modules/utils/error-handler');
 
-db.query(`INSERT INTO user_view_count (viewing_user, view_count) VALUES ('roger85', 1) ON CONFLICT (viewing_user) DO UPDATE SET view_count = user_view_count.view_count + 1`);
+db.query(`INSERT INTO user_view_count (viewing_user, view_count) VALUES ('roger85', 1) ON CONFLICT (viewing_user) DO UPDATE SET view_count = user_view_count.view_count + 1`)
+.catch(err => {
+    error.log(err);
+});
